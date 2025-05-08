b5ee81aee0a4c380be362c5406120ca188f9cf10
Jochen Bernard <jochenbernard@gmail.com>
2025-05-08 17:50:13 +0200

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,20 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "AsyncSnapshotTesting",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AsyncSnapshotTesting",
-            targets: ["AsyncSnapshotTesting"]),
+            targets: ["AsyncSnapshotTesting"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AsyncSnapshotTesting"),
+        .target(name: "AsyncSnapshotTesting"),
         .testTarget(
             name: "AsyncSnapshotTestingTests",
             dependencies: ["AsyncSnapshotTesting"]
-        ),
+        )
     ]
 )
